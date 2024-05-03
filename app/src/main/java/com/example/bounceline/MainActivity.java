0b19819e22fd424755262666b3e19d0a8fe103a5
YalokinBoom <yalokinboom@gmail.com>
2024-05-04 00:01:19 +0200

    Game loop

--- app/src/main/java/com/example/bounceline/MainActivity.java
+++ app/src/main/java/com/example/bounceline/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.bounceline;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * MainActivity is the entry point to our application.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set window to fullscreen (will hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game, so that objects in the Game class can be rendered to screen
+        setContentView(new Game(this));
     }
 }
